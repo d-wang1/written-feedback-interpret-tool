3c05273a29b39ee65cb6662133c11fb46681ebae
zjrken <zjrken@umich.edu>
2026-03-16 19:10:29 -0400

    cors

--- server/app/main.py
+++ server/app/main.py
@@ -44,7 +44,13 @@ def get_client():
 app = FastAPI(title="Written Feedback Interpretation API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000"],  
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+        "http://34.132.214.188:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
